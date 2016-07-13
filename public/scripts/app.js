99ed249308ba48ac3237757629898b4a83e5421a
Jose Zuniga <jramon.zu@gmail.com>
2016-07-13 15:32:32 -0700

    Add marker script

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,10 +11,33 @@ $(() => {
 });
 
 
-let map;
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.2827 , lng: -123.1207},
-    zoom: 15
+  var vancouver = {lat: 49.2827 , lng: -123.1207};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: vancouver
+  });
+
+  // This event listener calls addMarker() when the map is clicked.
+  google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng, map);
+  });
+
+  // Add a marker at the center of the map.
+  addMarker(vancouver, map);
+}
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+  // Add the marker at the clicked location, and add the next-available label
+  // from the array of alphabetical characters.
+  var marker = new google.maps.Marker({
+    position: location,
+    label: labels[labelIndex++ % labels.length],
+    map: map
   });
 }
+
