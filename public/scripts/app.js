17af8694178cf2064ca7fdd4e46a81752877d365
Sam Dunner <samdunner23@gmail.com>
2016-07-13 14:51:18 -0700

    Move map function to app.js file from index.html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,15 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+
 });
 
 
+let map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.2827 , lng: -123.1207},
+    zoom: 15
+  });
+}
