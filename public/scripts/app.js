dea98d6605de7a9b4bff5aac6d03411158931513
Jose Zuniga <jramon.zu@gmail.com>
2016-07-13 14:31:44 -0700

    Backtrack to working maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,10 +9,4 @@ $(() => {
   });;
 });
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 49.2827 , lng: -123.1207},
-    zoom: 15
-  });
-}
+
