3dbd929c73489682ac6a6103ac2e30b9231b56b5
Jose Zuniga <jramon.zu@gmail.com>
2016-07-13 14:17:23 -0700

    html scripts added to file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,18 @@
 $(() => {
-    $.ajax({
-      method: "GET",
-      url: "/api/users"
-    }).done((users) => {
-      for(user of users) {
-        $("<div>").text(user.name).appendTo($("body"));
-      }
-    });;
+  $.ajax({
+    method: "GET",
+    url: "/api/users"
+  }).done((users) => {
+    for(user of users) {
+      $("<div>").text(user.name).appendTo($("body"));
+    }
+  });;
 });
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.2827 , lng: -123.1207},
+    zoom: 15
+  });
+}
