b2ec9165dde5b0ff692714ceda302268b4206881
Jose Zuniga <jramon.zu@gmail.com>
2016-07-14 13:28:52 -0700

    Preference form added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -53,6 +53,18 @@ function addMarker(location, map) {
 });
 }
 
+function guardar() {
+  var name = document.getElementById('name').value;
+  var price = document.getElementById('price').value;
+  var style = document.getElementById('style').value;
+  var rating = document.getElementById('rating').value;
+  var latlong = localStorage.getItem('latlong');
+
+  localStorage.setItem('name'+contador, name);
+  localStorage.setItem('price'+contador, price);
+  localStorage.setItem('style'+contador, style);
+}
+
 
 
 
